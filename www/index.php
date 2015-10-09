a95087ce63c105401466047f006b39f51537a68d
resojlda <resojlda@gmail.com>
2015-10-09 19:23:07 +0300

    Added registration, sign in, sign out and some style

--- www/index.php
+++ www/index.php
@@ -1,21 +1,41 @@
 <?
 header('Content-type: text/html; charset=utf-8');
+session_start();
 ?>
 <!DOCTYPE html>
 <html ng-app lang="en">
 <head>
-	<style type="text/css">
-      html, body { height: 100%; margin: 0; padding: 0; }
-    </style>
     <title>Я карта, я карта, я карта!</title>
 	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDE0G3dQnsjvZCkTF_6KR48M6hcN-8HuBM&callback=initMap"></script>
 	<script type="text/javascript" src="js/main.js"></script>
+	<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,500italic,700,400italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="css" href="css/style.css" media="all">
 </head>
 <body>
-
-		<div id="map" style="height:100%;width:70%;float:left;"></div>
-		<div id="list" style="height:99%;width:30%;float:right;"><br><br><p id="plist" style="text-align:center;">Я - список ресторанов.</div>
-
-
+	<div id="signup-panel">
+		<img id="logo" src="img/logo.png">
+		<div id="afterlogoname">#RestaurantManager</div>
+		<?
+		if (isset($_SESSION['username'])) {
+			$file = 'logged.html';
+			$file_contents = file_get_contents($file);
+			$file_contents = str_replace("#username", $_SESSION['username'], $file_contents);
+			echo $file_contents;
+		}
+		else {
+			$file = 'notlogged.html';
+			$file_contents = file_get_contents($file);
+			echo $file_contents;
+		}
+		?>
+	</div>
+	<div id="map"></div>
+	<div id="list">
+		<br><br>
+		<div id="plist" class="listElement">
+			<br><br>Я - список ресторанов.
+		</div>
+	</div>
 </body>
 </html>
--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,8 @@
-var map;
+	var map;
 	function initMap() {
 		map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: -34.397, lng: 150.644},
-		zoom: 13
+		zoom: 15
 		});
 		var infoWindow = new google.maps.InfoWindow({map: map});
 		if (navigator.geolocation) {
@@ -49,4 +49,71 @@ var map;
 							'Не удалось определить Ваше местоположение.' :
 							'Ваш браузер не поддерживает автоматическое определение Вашего местоположения.'
 							);
+	}
+	
+	function confirmReg() {
+		var Login=document.getElementById('fieldLogin').value;
+		var Password=document.getElementById('fieldPassword').value;
+		var PasswordConfirm=document.getElementById('fieldPasswordConfirm').value;
+		if (Login==""||Password==""||PasswordConfirm=="") alert("Заполните все поля!");
+		else {
+			if (Login.match(/^[a-z0-9]+$/i)&&Password.match(/^[a-z0-9]+$/i)) {
+				if (Password==PasswordConfirm) {
+					$.ajax({
+						method: 'post',
+						url: 'register.php',
+						data: {
+						'Login': Login,
+						'Password': Password,
+						'ajax': true
+						},
+						success: function(data) {
+							if (data==1) {
+								alert("Регистрация успешна!");
+								window.location.href='index.php';
+							}
+							if (data==-1) alert("Такой логин уже зарегистрирован!");
+						}
+					});
+				}
+				else alert("Пароли должны совпадать!");
+			}
+			else alert("Поля могут содержать только цифры и латинские буквы!");
+		}
+	}
+	
+	function callReg() {
+		window.location.href='reg.php';
+	}
+	
+	function signOut() {
+		$.ajax({
+			method: 'post',
+			url: 'signout.php',
+			data: {
+			'ajax': true
+			},
+			success: function(data) {
+				location.reload();
+			}
+		});
+	}
+	
+	function signIn() {
+		var Login=document.getElementById('fieldLogin').value;
+		var Password=document.getElementById('fieldPassword').value;
+		if (Login==""||Password=="") alert("Заполните все поля!");
+		$.ajax({
+			method: 'post',
+			url: 'signin.php',
+			data: {
+			'Login': Login,
+			'Password': Password,
+			'ajax': true
+			},
+			success: function(data) {
+				if (data==1) location.reload();
+				if (data==-1) alert("Неправильный логин/пароль!");
+			}
+		});
 	}
